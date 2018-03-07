173817c717eca1556f65cd7966e064fb8713c2df
Jacob Albright <jacob.albright23@gmail.com>
2018-03-07 16:12:04 -0600

    set up router and server.js

--- server/server.js
+++ server/server.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const gameHistoryRouter = require('./routes/game-history');
+
+app.use(bodyParser.json());
+
+app.use('/games', gameHistoryRouter);
+
+app.use(express.static('server/public'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Server ready on port:', port);
+});
